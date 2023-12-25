090ed88161ac8955774199f97ea53e15cd8785fd
TAWSEEF <TAWSEEFSHAMIM@gmail.com>
2023-12-25 14:44:22 +0530

    done

--- q2.py
+++ q2.py
@@ -1,1 +1,12 @@
-print('hello')
+# Write a program which can compute the factorial of a given numbers.
+# The results should be printed in a comma-separated sequence on a single line.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# 40320
+num =8
+fact=1
+while (num>0):
+   fact=fact*num
+   num-=1
+print(fact)
